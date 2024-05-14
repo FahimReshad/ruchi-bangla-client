b86ad8e2d9a62230a1766dd0780466ab41288556
Md Fahim Hossain <fahimreshad3@gmail.com>
2024-05-15 04:48:14 +0600

    footer section added on the website

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/shared/Navbar";
+import Footer from "../Components/shared/Footer";
 
 const MainLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
